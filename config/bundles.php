0d9fbcf7fbf2b5bbbc70078f723fd5a00a088a80
Dawid <dawidbury1998@gmail.com>
2020-12-13 11:26:48 +0100

    Add mailer and twig

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
